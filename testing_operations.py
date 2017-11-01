e2e7cfff140480c348b5c6cbb78f577c2bf18869
cravingdata <32245139+cravingdata@users.noreply.github.com>
2017-10-31 21:48:32 -0400

    Add files via upload
    tested int, float, str, **, /, *, and combining strings through concatenations.

---
+++ testing_operations.py
@@ -0,0 +1,10 @@
+print int(3.14)
+print float(63/45)
+print str(88888888888)
+print 2 ** 3 ** 2
+print (2 ** 3) ** 2
+
+fruit = "banana"
+baked_good = " nut bread"
+print (fruit + baked_good)
+print ('fun' * 3)
